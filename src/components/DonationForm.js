456a4c5890bb5092c63384e87490be940e00ceb4
ahuzgb <ahuzgb@gmail.com>
2023-01-30 15:36:41 +0100

    donation form

--- src/components/DonationForm.js
+++ src/components/DonationForm.js
@@ -1,8 +1,112 @@
+import React, { useState } from "react";
+
 const DonationForm = () => {
+  const [articleName, setArticleName] = useState("");
+  const [expirationDate, setExpirationDate] = useState("");
+  const [weight, setWeight] = useState(0);
+  const [quantity, setQuantity] = useState(0);
+  const [category, setCategory] = useState("");
+  const [error, setError] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const categories = [
+    "Bread",
+    "Milk(haltbare)",
+    "Coffee",
+    "Pasta",
+    "Soup",
+    "Rice",
+    "Canned goods",
+    "Chocolate",
+  ];
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    setIsLoading(true);
+    setError(null);
+
+    const response = await fetch("http://localhost:8080/donations", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        article_name: articleName,
+        expiration_date: expirationDate,
+        weight,
+        quantity,
+        category,
+      }),
+    });
+
+    const data = await response.json();
+
+    if (!response.ok) {
+      setIsLoading(false);
+      setError(data.error);
+    }
+
+    if (response.ok) {
+      setIsLoading(false);
+      alert("Donation successfully added");
+      resetForm();
+    }
+  };
+  const resetForm = () => {
+    setArticleName("");
+    setExpirationDate("");
+    setWeight(0);
+    setQuantity(0);
+    setCategory("");
+  };
+
   return (
-    <div>
-      <p>form</p>
-    </div>
+    <form className="donation-form" onSubmit={handleSubmit}>
+      <h3>Donation Form</h3>
+      <label>Article name: </label>
+      <input
+        type="text"
+        onChange={(e) => setArticleName(e.target.value)}
+        value={articleName}
+      />
+
+      <label>Expiration date: </label>
+      <input
+        type="text"
+        onChange={(e) => setExpirationDate(e.target.value)}
+        value={expirationDate}
+      />
+
+      <label>Weight: </label>
+      <input
+        type="number"
+        onChange={(e) => setWeight(e.target.value)}
+        value={weight}
+      />
+
+      <label>Quantity: </label>
+      <input
+        type="number"
+        onChange={(e) => setQuantity(e.target.value)}
+        value={quantity}
+      />
+
+      <label>Category: </label>
+      {categories.map((cat) => (
+        <div key={cat}>
+          <input
+            type="radio"
+            name="category"
+            id={cat}
+            value={cat}
+            checked={category === cat}
+            onChange={(e) => setCategory(e.target.value)}
+          />
+          <label htmlFor={cat}>{cat}</label>
+        </div>
+      ))}
+      <button>Submit</button>
+      {error && <div className="error">{error}</div>}
+    </form>
   );
 };
 
