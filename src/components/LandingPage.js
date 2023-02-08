a7fd151df2e0df27be6a1683c338ebe45c79d76a
Milorad Stojanov <miloradstojanov93@gmail.com>
2023-02-08 12:26:16 +0100

    landingPage

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,7 +1,14 @@
+import "../components/landingPage.css";
+import Picture from "./images/background-image.jpg";
 const LandingPage = () => {
   return (
-    <div className="landingPage">
-      <h1>Landing Page</h1>
+    <div className="container-landingPage">
+      <div className="header-landingPage">
+        <h1 className="header-title">
+          Join the foodbär community
+          <br /> and make sure good food gets eaten, not wasted.
+        </h1>
+      </div>
     </div>
   );
 };
