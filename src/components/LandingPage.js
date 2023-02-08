c5815ddf55b969b82be1ddfb9268f7278a60914e
Milorad Stojanov <miloradstojanov93@gmail.com>
2023-02-08 14:35:05 +0100

    landingpage

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -4,13 +4,21 @@ const LandingPage = () => {
   return (
     <div className="container-landingPage">
       <div className="header-landingPage">
-        <h1 className="header-title">
-          Join the foodbär community
-          <br /> and make sure good food gets eaten, not wasted.
-        </h1>
+        <div>
+          <h1 className="header-title">Join the foodbär community</h1>
+          <h1 className="header-title">and make sure good food</h1>
+          <h1 className="header-title">gets eaten, not wasted.</h1>
+        </div>
       </div>
     </div>
   );
 };
 
 export default LandingPage;
+
+{
+  /* <h1 className="header-title">
+          Join the foodbär community
+          <br /> and make sure good food gets eaten, not wasted.
+        </h1> */
+}
